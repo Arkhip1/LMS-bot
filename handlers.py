ecce36c29757e98efa075e57fd72e44c0482cdce
Arkhip1 <ar9031268843@gmail.com>
2024-04-23 06:06:56 +0300

    final

--- handlers.py
+++ handlers.py
@@ -1,12 +1,19 @@
-from aiogram import F, Router
+from aiogram import F, Router, Bot
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+import time
+import schedule
 
 import keyboards as kbs
 
 router = Router()
+
+
+class Reminds(StatesGroup):
+    remind1 = State()
+    remind2 = State()
 
 
 class Register(StatesGroup):
@@ -15,9 +22,13 @@ class Register(StatesGroup):
     fav_color = State()
 
 
+def send_message(message):
+    bot.send_message(message.chat.id, 'fsgdhdfs')
+
+
 @router.message(CommandStart())
 async def cmnd_strt(message: Message, state: FSMContext):
-    await message.answer('Привет! Я помогу тебе не забыть о важных событиях:) \nПриступим?')
+    await message.answer('Привет! Я помогу вам не забыть о важных событиях:) \nПриступим?')
     await state.set_state(Register.name)
     await message.answer('Введите своё имя')
 
@@ -41,10 +52,49 @@ async def fc(message: Message, state: FSMContext):
     await state.update_data(fav_color=message.text)
     data = await state.get_data()
     with open('data.txt', mode='w', encoding='utf8') as dt:
-        dt.write(f'Имя: {data["name"]}, возраст: {data["age"]}, любимый цвет: {data["fav_color"]}.')
+        dt.write(f'Имя: {data["name"]}; Возраст: {data["age"]}; Любимый цвет: {data["fav_color"]}.')
+        dt.close()
+    await state.clear()
+    await state.set_state(Reminds.remind1)
+    await message.answer('Какое напоминание создать?')
+
+
+@router.message(Reminds.remind1)
+async def set_remind(message: Message, state: FSMContext):
+    await state.update_data(remind1=message.text)
+    data = await state.get_data()
+    with open('data.txt', mode='a', encoding='utf8') as dt:
+        dt.write(f'\n{data["remind1"]}')
+        dt.close()
+    await message.answer('Времени осталось:', reply_markup=kbs.time)
     await state.clear()
 
 
-@router.message()
-async def start_working(message: Message):
+@router.message(F.text == '15 минут')
+async def set_remind(message: Message, state: FSMContext):
+    await message.answer('Заметка успешно создана! Напоминание произойдет через 15 минут')
+    await state.set_state(Reminds.remind1)
     await message.answer('Какое напоминание создать?')
+
+
+@router.message(F.text == '1 час')
+async def set_remind(message: Message, state: FSMContext):
+    await message.answer('Заметка успешно создана! Напоминание произойдет через 1 час')
+    await state.set_state(Reminds.remind1)
+    await message.answer('Какое напоминание создать?')
+
+
+@router.message(F.text == '1 день')
+async def set_remind(message: Message, state: FSMContext):
+    await message.answer('Заметка успешно создана! Напоминание произойдет через 1 день')
+    await state.set_state(Reminds.remind1)
+    await message.answer('Какое напоминание создать?')
+
+
+@router.message(F.text == '1 неделя')
+async def set_remind(message: Message, state: FSMContext):
+    await message.answer('Заметка успешно создана! Напоминание произойдет через 1 неделю')
+    await state.set_state(Reminds.remind1)
+    await message.answer('Какое напоминание создать?')
+
+
--- keyboards.py
+++ keyboards.py
@@ -2,3 +2,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 strt = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='/start')]],
                            resize_keyboard=True)
+
+
+time = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='15 минут'),
+                                      KeyboardButton(text='1 час')],
+                                     [KeyboardButton(text='1 день'),
+                                      KeyboardButton(text='1 неделя')]],
+                           resize_keyboard=True)
